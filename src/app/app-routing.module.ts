30d704c09f11ba2edfa9d9360508263e67d9c4d6
GabrielValde <gabriel.valdez@pentcloud.com>
2022-11-17 19:01:46 -0600

    Agregue la vista de heroes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes,RouterModule } from '@angular/router';
+import { HeroesComponent } from './componentes/heroes/heroes.component';
 
+const routes:Routes=[
+  {path:"Heroes", component:HeroesComponent}
 
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+      RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
